046bacdd97d95d3c9d352a0db0965dd08e23214c
TristanKalvoda <kalvo007@morris.umn.edu>
2025-04-24 18:12:40 -0500

    vector_add in C and CUDA

---
+++ vector_add.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+#define N 10000000
+
+void vector_add(float *out, float *a, float *b, int n) {
+    for(int i = 0; i < n; i++){
+        out[i] = a[i] + b[i];
+    }
+}
+
+int main(){
+    float *a, *b, *out; 
+
+    // Allocate memory
+    a   = (float*)malloc(sizeof(float) * N);
+    b   = (float*)malloc(sizeof(float) * N);
+    out = (float*)malloc(sizeof(float) * N);
+
+    // Initialize array
+    for(int i = 0; i < N; i++){
+        a[i] = 1.0f; b[i] = 2.0f;
+    }
+
+    // Main function
+    vector_add(out, a, b, N);
+
+    // Print first 10 results
+    printf("First 10 results:\n");
+    for (int i = 0; i < 10; i++) {
+        printf("out[%d] = %f\n", i, out[i]);
+    }
+    printf("Complete\n");
+}
---
+++ vector_add.cu
@@ -0,0 +1,30 @@
+#define N 10000000
+
+__global__ void vector_add(float *out, float *a, float *b, int n) {
+    for(int i = 0; i < n; i++){
+        out[i] = a[i] + b[i];
+    }
+}
+
+int main(){
+    float *a, *b, *out; 
+
+    // Allocate memory
+    a   = (float*)malloc(sizeof(float) * N);
+    b   = (float*)malloc(sizeof(float) * N);
+    out = (float*)malloc(sizeof(float) * N);
+
+    // Initialize array
+    for(int i = 0; i < N; i++){
+        a[i] = 1.0f; b[i] = 2.0f;
+    }
+
+    // Main function
+    vector_add<<<1,1>>>(out, a, b, N);
+    // Print first 10 results
+    printf("First 10 results:\n");
+    for (int i = 0; i < 10; i++) {
+        printf("out[%d] = %f\n", i, out[i]);
+    }
+    printf("Complete\n");
+}
